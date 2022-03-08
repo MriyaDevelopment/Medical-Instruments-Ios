280b22b63c9a03f5284588fbe9a34644df974fb3
Denis Klyev <denis63982013@gmail.com>
2022-03-08 15:04:00 +0400

    тест

---
+++ MedicalInstruments/UI/Screens/MainScreen/View/Cell/MainCollectionViewCell.swift
@@ -0,0 +1,112 @@
+//
+//  MainCollectionViewCell.swift
+//  MedicalInstruments
+//
+//  Created by Mac Pro on 08.03.2022.
+//
+
+import UIKit
+
+final class MainCollectionViewCell: UICollectionViewCell {
+    
+    private var backgroundImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = MainFont.medium(size: 12)
+        label.textColor = BaseColor.hex_232324.uiColor()
+        label.text = "Общая хирургия"
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    private var subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = MainFont.medium(size: 10)
+        label.textColor = BaseColor.hex_232324.uiColor()
+        label.text = "Инструментарий: 40"
+        return label
+    }()
+    
+    private var imageView: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleAspectFill
+        return image
+    }()
+    
+    private var nextImageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        layer.cornerRadius = 14
+        clipsToBounds = true
+        
+        addElements()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func configure(item: MainStruct) {
+        self.titleLabel.text = item.titleText
+        self.subtitleLabel.text = item.subtitleText
+        self.backgroundImageView.image = item.backgroundImage
+        self.imageView.image = item.iconImage
+    }
+
+    
+    private func addElements() {
+        
+        addSubview(backgroundImageView)
+        backgroundImageView.addSubview(titleLabel)
+        backgroundImageView.addSubview(subtitleLabel)
+        backgroundImageView.addSubview(imageView)
+        backgroundImageView.addSubview(nextImageView)
+        
+        makeConstraints()
+    }
+    
+    private func makeConstraints() {
+        
+        backgroundImageView.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
+        
+        imageView.snp.makeConstraints { (make) in
+            make.left.equalToSuperview().inset(12)
+            make.top.equalToSuperview().inset(15)
+            make.height.width.equalTo(43)
+        }
+        
+        nextImageView.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(15)
+            make.right.equalToSuperview().offset(-14)
+        }
+        
+        titleLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(imageView.snp.bottom).offset(5)
+            make.left.equalToSuperview().inset(12)
+            make.right.equalToSuperview()
+        }
+        
+        subtitleLabel.snp.makeConstraints { (make) in
+            make.left.equalToSuperview().inset(12)
+            make.right.bottom.equalToSuperview().inset(14)
+        }
+        
+    }
+}
+
+//Click Animation
+
+//extension MainCollectionViewCell: AnimationCollectionViewCellProtocol {
+//
+//}
--- MedicalInstruments/UI/Screens/MainScreen/ViewController/MainViewController.swift
+++ MedicalInstruments/UI/Screens/MainScreen/ViewController/MainViewController.swift
@@ -21,6 +21,43 @@ final class MainViewController<View: MainView>: BaseViewController<View> {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureCollectionView()
     }
 
+    private func configureCollectionView(){
+        var elements: [MainStruct] = []
+        elements.append(MainStruct.init(id: 1,
+                                        backgroundImage: AppIcons.getIcon(.i_default_image),
+                                        iconImage: AppIcons.getIcon(.i_default_image),
+                                        titleText: "Общая хирургия",
+                                        subtitleText: "Инструментарий: 40"))
+        elements.append(MainStruct.init(id: 2,
+                                        backgroundImage: AppIcons.getIcon(.i_default_image),
+                                        iconImage: AppIcons.getIcon(.i_default_image),
+                                        titleText: "Стоматология",
+                                        subtitleText: "Инструментарий: 40"))
+        elements.append(MainStruct.init(id: 3,
+                                        backgroundImage: AppIcons.getIcon(.i_default_image),
+                                        iconImage: AppIcons.getIcon(.i_default_image),
+                                        titleText: "Акушерство и гинекология",
+                                        subtitleText: "Инструментарий: 40"))
+        elements.append(MainStruct.init(id: 4,
+                                        backgroundImage: AppIcons.getIcon(.i_default_image),
+                                        iconImage: AppIcons.getIcon(.i_default_image),
+                                        titleText: "Нейрохирургия",
+                                        subtitleText: "Инструментарий: 40"))
+        elements.append(MainStruct.init(id: 5,
+                                        backgroundImage: AppIcons.getIcon(.i_default_image),
+                                        iconImage: AppIcons.getIcon(.i_default_image),
+                                        titleText: "Офтальмология",
+                                        subtitleText: "Инструментарий: 40"))
+        elements.append(MainStruct.init(id: 6,
+                                        backgroundImage: AppIcons.getIcon(.i_default_image),
+                                        iconImage: AppIcons.getIcon(.i_default_image),
+                                        titleText: "Оториноларингология",
+                                        subtitleText: "Инструментарий: 40"))
+
+        rootView.configure(elements: elements)
+    }
 }
